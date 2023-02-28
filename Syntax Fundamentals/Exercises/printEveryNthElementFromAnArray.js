49388d9dd222794a93be3386b2a9b729b2422a38
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2023-02-28 18:11:09 +0200

    Add files via upload

---
+++ Syntax Fundamentals/Exercises/printEveryNthElementFromAnArray.js
@@ -0,0 +1,9 @@
+function solve(array, step) {
+  let result = [];
+  for (let i = 0; i < array.length; i += step) {
+    result.push(array[i]);
+  }
+  return result;
+}
+
+console.log(solve(["5", "20", "31", "4", "20"], 2));
