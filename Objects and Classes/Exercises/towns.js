f32c93b436748ea10e317270b3b3c2d80c19f48e
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2023-03-13 17:49:34 +0200

    Add files via upload

---
+++ Objects and Classes/Exercises/towns.js
@@ -0,0 +1,9 @@
+function solve(array) {
+  for (const element of array) {
+    let [town, latitude, longitude] = element.split(" | ");
+    latitude = Number(latitude).toFixed(2);
+    longitude = Number(longitude).toFixed(2);
+    console.log({ town, latitude, longitude });
+  }
+}
+solve(["Sofia | 42.696552 | 23.32601", "Beijing | 39.913818 | 116.363625"]);
