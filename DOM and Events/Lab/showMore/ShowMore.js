5f1ef85d7a9e0ec066a75a4ff079bcb2dd54068f
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2023-03-20 20:13:58 +0200

    Add files via upload

---
+++ DOM and Events/Lab/showMore/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+  let link = document.getElementById("more");
+  link.style.display = "none";
+  let text = document.getElementById("text");
+  text.style.display = "inline";
+}
