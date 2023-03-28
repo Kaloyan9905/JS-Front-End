781c5a9736fc8b7771abb1c65896b2f5b24914ec
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2023-03-28 17:01:17 +0300

    Delete subtract.js

--- DOM and Events/Exercises/subtract.js
+++
@@ -1,7 +0,0 @@
-function subtract() {
-  let firstNumber = document.getElementById("firstNumber").value;
-  let secondNumber = document.getElementById("secondNumber").value;
-
-  document.getElementById("result").textContent =
-    Number(firstNumber) - Number(secondNumber);
-}
